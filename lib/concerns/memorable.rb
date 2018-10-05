7e04dfe48bc931b1248e22bc5a3f7f230a724cdf
Harry Bloch <harryfbloch@gmail.com>
2018-10-05 18:55:46 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,16 +1,17 @@
 require 'pry'
 module Memorable
   module ClassMethods
-  def reset_all 
-    self.all.clear
+    def reset_all 
+      self.all.clear
+    end
+    
+    def count
+      self.all.count
+    end
   end
-  
-  def count
-    self.all.count
-  end
-end
-module InstanceMethods
-  def initialize
-    self.class.all << self
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 end
